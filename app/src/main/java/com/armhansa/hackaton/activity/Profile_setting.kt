95f57d645db11c47666ed56da24f389ae96065a8
Prapasiri Lertkriangkraiying <prapasiri.l@ku.th>
2019-10-03 20:52:19 +0700

    Add profile Setting

---
+++ app/src/main/java/com/armhansa/hackaton/activity/Profile_setting.kt
@@ -0,0 +1,13 @@
+package com.armhansa.hackaton.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.armhansa.hackaton.R
+
+class Profile_setting : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_setting)
+    }
+}
